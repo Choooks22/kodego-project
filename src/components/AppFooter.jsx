bdb8208d972450ab4e752ef6e6d4cae3acef8bce
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-11-30 23:26:51 +0800

    Add social media icons to footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,10 @@
-import Toolbar from '@mui/material/Toolbar';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 
 export default function AppFooter() {
   return (
@@ -7,10 +12,20 @@ export default function AppFooter() {
       component="footer"
       className="AppFooter"
       position="static"
-      color="primary"
       sx={{ top: 'auto', bottom: 0 }}
     >
       <Toolbar>
+        <Box sx={{ display: 'flex', gap: '1rem', margin: 'auto' }}>
+          <IconButton>
+            <GitHubIcon sx={{ fontSize: 30 }} />
+          </IconButton>
+          <IconButton>
+            <TwitterIcon sx={{ fontSize: 30 }} />
+          </IconButton>
+          <IconButton>
+            <FacebookIcon sx={{ fontSize: 30 }} />
+          </IconButton>
+        </Box>
       </Toolbar>
     </AppBar>
   );
