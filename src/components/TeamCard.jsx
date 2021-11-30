612a4356a247c227c2222af9b6c73ad655281bf8
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-11-30 14:28:23 +0800

    Change team card layout

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,35 +1,31 @@
-import Avatar from '@mui/material/Avatar';
+import './TeamCard.scss';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
-import GitHubIcon from './GitHubIcon';
 
 export default function TeamCard(props) {
   return (
-    <Card className="TeamCard" sx={{ maxWidth: '27.5rem' }}>
-      <CardHeader
-        avatar={<Avatar src={props.icon} />}
-        title={props.name}
-        subheader={props.title}
-        action={<GitHubIcon username={props.username} />}
-      />
+    <Card className="TeamCard">
+      <Box sx={{ display: 'flex', flexFlow: 'column', justifyContent: 'space-between' }}>
+        <CardHeader
+          title={props.name}
+          subheader={props.title}
+        />
+        <CardActions>
+          <Button href={`https://github.com/${props.username}`} target="_blank">GitHub</Button>
+          <Button href={`https://github.com/${props.username}`} target="_blank">Portfolio</Button>
+        </CardActions>
+      </Box>
       <CardMedia
         component="img"
-        image={props.project.preview}
+        className="TeamCardImage"
+        image={props.icon}
+        sx={{ width: '10.5rem' }}
       />
-      <CardContent>
-        <Typography variant="h5" gutterBottom>{props.project.title}</Typography>
-        <Typography variant="body2" color="text.secondary">{props.project.desc}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button color="inherit" href={props.project.link} target="_blank">Project</Button>
-        <Button color="inherit" href={props.portfolio} target="_blank">Portfolio</Button>
-      </CardActions>
     </Card>
   );
 }
@@ -39,11 +35,5 @@ TeamCard.propTypes = {
   name: PropTypes.string.isRequired,
   username: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  project: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    preview: PropTypes.string.isRequired,
-    desc: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-  }).isRequired,
   portfolio: PropTypes.string.isRequired,
 };
