bf61b56d7b5d4e94f57c33318c62719f38d23f44
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-12-01 09:54:05 +0800

    Adjust margins

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -6,22 +6,20 @@ import './Services.scss';
 
 export default function IntroductionPage() {
   return (
-    <>
-      <Container maxWidth="md" my={2}>
-        <Typography variant="h2" sx={{ textAlign: 'center' }} my={8}>
+    <Container maxWidth="md" my={2}>
+      <Typography variant="h2" sx={{ textAlign: 'center' }} mb={8}>
           Services Offered
-        </Typography>
-        <Box sx={{ display: 'flex', gap: '1rem', justifyContent: 'center' }}>
-          <ServiceCard
-            image="https://images.idgesg.net/images/article/2019/05/team_manipulates_coding_elements_programming_interface_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056786_2400x1600-100795794-large.jpg?auto=webp&quality=85,70"
-            title="Software Services"
-          />
-          <ServiceCard
-            image="https://4.imimg.com/data4/OR/AJ/MY-32248659/all-kind-of-computer-hardware-services-500x500.jpg"
-            title="Hardware Services"
-          />
-        </Box>
-      </Container>
-    </>
+      </Typography>
+      <Box sx={{ display: 'flex', gap: '1rem', justifyContent: 'center' }}>
+        <ServiceCard
+          image="https://images.idgesg.net/images/article/2019/05/team_manipulates_coding_elements_programming_interface_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056786_2400x1600-100795794-large.jpg?auto=webp&quality=85,70"
+          title="Software Services"
+        />
+        <ServiceCard
+          image="https://4.imimg.com/data4/OR/AJ/MY-32248659/all-kind-of-computer-hardware-services-500x500.jpg"
+          title="Hardware Services"
+        />
+      </Box>
+    </Container>
   );
 }
